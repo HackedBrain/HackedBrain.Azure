8d72d86bee103305c527f2f527f2f209dd589c05
Drew Marsh <drub0y@hotmail.com>
2013-08-14 21:26:04 -0700

    - new fangled way to do Observable implementation

--- HackedBrain.Azure.ServiceBus.Tests/MessageReceiverExtentions.cs
+++ HackedBrain.Azure.ServiceBus.Tests/MessageReceiverExtentions.cs
@@ -3,33 +3,54 @@ using Microsoft.ServiceBus;
 using Microsoft.ServiceBus.Messaging;
 using Moq;
 using Xunit;
+using System.Reactive.Linq;
+using System.Reactive.Concurrency;
 
 namespace HackedBrain.WindowsAzure.ServiceBus.Messaging.Tests
 {
-	public class MessageReceiverObservableExtensionsTests
+	public class MessageReceiverObservableExtensionsTests : IDisposable
 	{
-		public class AsObservableFacts : IDisposable
+		private NamespaceManager namespaceManager;
+		private QueueDescription queueDescription;
+		private MessageSender messageSender;
+		private MessageReceiver messageReceiver;
+
+		public MessageReceiverObservableExtensionsTests()
 		{
-			private MessageReceiver messageReceiver;
+			TokenProvider tp = TokenProvider.CreateSharedSecretTokenProvider("owner", "");
 
-			public AsObservableFacts()
-			{
-				TokenProvider tp = TokenProvider.CreateSharedSecretTokenProvider("owner", new byte[] { 1, 2, 3 });
-
-				MessagingFactorySettings mfs = new MessagingFactorySettings
+			this.namespaceManager = new NamespaceManager(
+				"",
+				new NamespaceManagerSettings
 				{
 					TokenProvider = tp
-				};
+				});
 
-				MessagingFactory mf = MessagingFactory.Create(new Uri("sb://unittest"), mfs);
-				this.messageReceiver = mf.CreateMessageReceiver("test-queue-path");
-			}
+			this.queueDescription = this.namespaceManager.CreateQueue("MessageReceiverObservableExtensionTests-" + Guid.NewGuid().ToString("N"));
 
-			public void Dispose()
-			{
-				
-			}
-			
+			MessagingFactory messagingFactory = MessagingFactory.Create(
+				this.namespaceManager.Address,
+				new MessagingFactorySettings
+				{
+					TokenProvider = tp
+				});
+
+			this.messageSender = messagingFactory.CreateMessageSender(this.queueDescription.Path);
+			this.messageReceiver = messagingFactory.CreateMessageReceiver(this.queueDescription.Path);
+		}
+
+		public void Dispose()
+		{
+			this.messageSender.Close();
+			this.messageReceiver.Close();
+
+			this.namespaceManager.DeleteQueue(this.queueDescription.Path);
+		}
+
+		public class AsObservableFacts : MessageReceiverObservableExtensionsTests
+		{
+
+
 			[Fact]
 			public void Should_Throw_ArgumentNullException_For_Null_MessageReceiver_Instance()
 			{
@@ -42,12 +63,49 @@ namespace HackedBrain.WindowsAzure.ServiceBus.Messaging.Tests
 			}
 
 			[Fact]
-			public void Should_Return_NonNull_MessageReceiverObservable_Instance()
+			public void Should_Return_NonNull_IObservable_Instance()
 			{
 				IObservable<BrokeredMessage> messageReceiverObservable = this.messageReceiver.AsObservable();
 
 				Assert.NotNull(messageReceiverObservable);
-				Assert.IsType<MessageReceiverObservable>(messageReceiverObservable);
+			}
+		}
+
+		public class ObservableMessageReceptionFacts : MessageReceiverObservableExtensionsTests
+		{
+			[Fact]
+			public void Should_Receive_Expected_Message()
+			{
+				string newMessageId = Guid.NewGuid().ToString();
+
+				this.messageSender.Send(new BrokeredMessage
+					{
+						MessageId = newMessageId
+					});
+
+				string receivedMessageId = this.messageReceiver.AsObservable(NewThreadScheduler.Default).Take(1).Select(bm => bm.MessageId).First();
+
+				Assert.Equal(newMessageId, receivedMessageId);
+			}
+
+			[Fact]
+			public void Should_Receive_Expected_Messages_In_Order()
+			{
+				string firstMessageId = Guid.NewGuid().ToString();
+
+				this.messageSender.Send(new BrokeredMessage
+				{
+					MessageId = firstMessageId
+				});
+
+				string secondMessageId = Guid.NewGuid().ToString();
+
+				this.messageSender.Send(new BrokeredMessage
+				{
+					MessageId = secondMessageId
+				});
+
+				Assert.True(this.messageReceiver.AsObservable(NewThreadScheduler.Default).Take(2).Select(bm => bm.MessageId).SequenceEqual(new[] { firstMessageId, secondMessageId }).First());
 			}
 		}
 	}
--- HackedBrain.Azure.ServiceBus/MessageReceiverExtensions.cs
+++ HackedBrain.Azure.ServiceBus/MessageReceiverExtensions.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.ServiceBus.Messaging;
 
 namespace HackedBrain.WindowsAzure.ServiceBus.Messaging
@@ -10,12 +14,78 @@ namespace HackedBrain.WindowsAzure.ServiceBus.Messaging
 	{
 		public static IObservable<BrokeredMessage> AsObservable(this MessageReceiver messageReceiver)
 		{
+			return messageReceiver.AsObservable(CancellationToken.None, Scheduler.Immediate);
+		}
+
+		public static IObservable<BrokeredMessage> AsObservable(this MessageReceiver messageReceiver, CancellationToken cancellationToken)
+		{
+			return messageReceiver.AsObservable(cancellationToken, Scheduler.Immediate);
+		}
+
+		public static IObservable<BrokeredMessage> AsObservable(this MessageReceiver messageReceiver, IScheduler scheduler)
+		{
+			return messageReceiver.AsObservable(CancellationToken.None, scheduler);
+		}
+
+		public static IObservable<BrokeredMessage> AsObservableFullAsync(this MessageReceiver messageReceiver, CancellationToken cancellationToken, IScheduler scheduler)
+		{
 			if(messageReceiver == null)
 			{
 				throw new ArgumentNullException("messageReceiver");
 			}
-			
-			return new MessageReceiverObservable(messageReceiver);
+
+			Func<IScheduler, CancellationToken, Task<IDisposable>> recursiveMessageReceiver = null;
+
+			return Observable.Create<BrokeredMessage>(
+				observer =>
+				{
+					recursiveMessageReceiver = async (recursiveScheduler, recursiveCancellationToken) =>
+					{
+						try
+						{
+							BrokeredMessage message = await messageReceiver.ReceiveAsync();
+
+							observer.OnNext(message);
+						}
+						catch(Exception exception)
+						{
+							observer.OnError(exception);
+						}
+
+						return recursiveScheduler.ScheduleAsync(recursiveMessageReceiver);
+					};
+
+					return scheduler.ScheduleAsync(recursiveMessageReceiver);
+				});
+		}
+
+		public static IObservable<BrokeredMessage> AsObservable(this MessageReceiver messageReceiver, CancellationToken cancellationToken, IScheduler scheduler)
+		{
+			if(messageReceiver == null)
+			{
+				throw new ArgumentNullException("messageReceiver");
+			}
+
+			return Observable.Create<BrokeredMessage>(
+				observer =>
+				{
+					return scheduler.Schedule(
+						recurse =>
+						{
+							try
+							{
+								BrokeredMessage message = messageReceiver.Receive();
+
+								observer.OnNext(message);
+							}
+							catch(Exception exception)
+							{
+								observer.OnError(exception);
+							}
+
+							scheduler.Schedule(recurse);
+						});
+				});
 		}
 	}
 }
