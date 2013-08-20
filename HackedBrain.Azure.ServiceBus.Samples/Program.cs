6dab908708d3809be1d1e519c273828caedb1129
Drew Marsh <drub0y@hotmail.com>
2013-08-20 08:06:43 -0700

    - created generic, func<T> based helper method in MessageReceiverExtensions that will allow reuse across classes that don't derive from MessageReceiver (e.g. QueueClient) - adding a test console app which will demo various ways to use the observable extensions

---
+++ HackedBrain.Azure.ServiceBus.Samples/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using HackedBrain.WindowsAzure.ServiceBus.Messaging;
+
+namespace HackedBrain.Azure.ServiceBus.Samples
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			NamespaceManager namespaceManager;
+			QueueDescription queueDescription;
+			QueueClient queueClient;
+
+			TokenProvider tp = TokenProvider.CreateSharedSecretTokenProvider("owner", "AEKOXTG5x21S1ZIdOl3m5xU+EfR+WOUGrxKsaqDfgD0=");
+
+			namespaceManager = new NamespaceManager(
+				"sb://dmarsh-msdn.servicebus.windows.net",
+				new NamespaceManagerSettings
+				{
+					TokenProvider = tp
+				});
+
+			string queuePath = "MessageReceiverConsoleApp";
+
+			if(namespaceManager.QueueExists(queuePath))
+			{
+				namespaceManager.DeleteQueue(queuePath);
+			}
+
+			queueDescription = new QueueDescription(queuePath)
+			{
+				RequiresSession = true,
+				DefaultMessageTimeToLive = TimeSpan.FromMinutes(5),
+			};
+
+			queueDescription = namespaceManager.CreateQueue(queueDescription);
+
+			MessagingFactory messagingFactory = MessagingFactory.Create(
+				namespaceManager.Address,
+				new MessagingFactorySettings
+				{
+					TokenProvider = tp
+				});
+
+			queueClient = messagingFactory.CreateQueueClient(queueDescription.Path);
+
+			Console.WriteLine("Which sample would you like to run?");
+			Console.WriteLine("  1. Receive message from sessions");
+
+			switch(Console.ReadKey(true).KeyChar)
+			{
+				case '1':
+					Program.ReceiveMessagesFromSessions(queueClient);
+
+					break;
+
+				default:
+					Console.WriteLine("Unknown option. Bye.");
+
+					break;
+			}
+		}
+
+		private static void ReceiveMessagesFromSessions(QueueClient queueClient)
+		{
+			Console.WriteLine("Receiving messages from sessions...");
+
+			CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+			Task readSessionMessages = Task.Run(() =>
+			{
+				for(MessageSession nextSession = queueClient.AcceptMessageSession(TimeSpan.FromSeconds(30)); !cancellationTokenSource.IsCancellationRequested && nextSession != null; nextSession = queueClient.AcceptMessageSession(TimeSpan.FromSeconds(30)))
+				{
+					nextSession.AsObservable().Subscribe(
+					bm =>
+					{
+						Console.WriteLine("Received Message: SessionId={0}, MessageId={1}", bm.SessionId, bm.MessageId);
+					},
+					cancellationTokenSource.Token);
+				}
+			},
+			cancellationTokenSource.Token);
+
+			Task writeSessionMessages = Task.Run(async () =>
+				{
+					int messageId = 0;
+					int sessionId = 0;
+
+					do
+					{
+						await queueClient.SendAsync(new BrokeredMessage
+						{
+							SessionId = sessionId.ToString(),
+							MessageId = messageId.ToString()
+						});
+
+						sessionId++;
+
+						if(sessionId % 4 == 0)
+						{
+							sessionId = 0;
+						}
+
+						messageId++;
+					} while(!cancellationTokenSource.Token.IsCancellationRequested);
+				},
+				cancellationTokenSource.Token);
+
+			Console.WriteLine("Press any key to stop...");
+			Console.ReadKey(true);
+
+			cancellationTokenSource.Cancel();
+
+			Console.WriteLine("Stopping...");
+
+			Task.WaitAll(readSessionMessages, writeSessionMessages);
+
+			Console.WriteLine("Stopped!");
+		}
+	}
+}
--- HackedBrain.Azure.ServiceBus/MessageReceiverExtensions.cs
+++ HackedBrain.Azure.ServiceBus/MessageReceiverExtensions.cs
@@ -36,20 +36,54 @@ namespace HackedBrain.WindowsAzure.ServiceBus.Messaging
 				throw new ArgumentNullException("messageReceiver");
 			}
 
-			IObservable<BrokeredMessage> brokeredMessages = Observable.Create<BrokeredMessage>(
-				observer =>
-				{
-					Func<Task<BrokeredMessage>> receiveCall = receiveWaitTime != TimeSpan.MinValue ? 
+			Func<Task<BrokeredMessage>> receiveMessage = receiveWaitTime != TimeSpan.MinValue ?
 						new Func<Task<BrokeredMessage>>(() => messageReceiver.ReceiveAsync(receiveWaitTime)) :
 						new Func<Task<BrokeredMessage>>(() => messageReceiver.ReceiveAsync());
 
+			return MessageReceiverExtensions.ObservableFromAsyncReceiverDetails(() => messageReceiver.IsClosed, () => messageReceiver.Close(), receiveMessage, scheduler);
+		}
+
+		public static IObservable<BrokeredMessage> AsObservable(this QueueClient queueClient)
+		{
+			return queueClient.AsObservable(TimeSpan.MinValue, ImmediateScheduler.Instance);
+		}
+
+		public static IObservable<BrokeredMessage> AsObservable(this QueueClient queueClient, IScheduler scheduler)
+		{
+			return queueClient.AsObservable(TimeSpan.MinValue, scheduler);
+		}
+
+		public static IObservable<BrokeredMessage> AsObservable(this QueueClient queueClient, TimeSpan receiveWaitTime)
+		{
+			return queueClient.AsObservable(receiveWaitTime, ImmediateScheduler.Instance);
+		}
+
+		public static IObservable<BrokeredMessage> AsObservable(this QueueClient queueClient, TimeSpan receiveWaitTime, IScheduler scheduler)
+		{
+			if(queueClient == null)
+			{
+				throw new ArgumentNullException("queueClient");
+			}
+
+			Func<Task<BrokeredMessage>> receiveMessage = receiveWaitTime != TimeSpan.MinValue ?
+						new Func<Task<BrokeredMessage>>(() => queueClient.ReceiveAsync(receiveWaitTime)) :
+						new Func<Task<BrokeredMessage>>(() => queueClient.ReceiveAsync());
+
+			return MessageReceiverExtensions.ObservableFromAsyncReceiverDetails(() => queueClient.IsClosed, () => queueClient.Close(), receiveMessage, scheduler);
+		}
+
+		private static IObservable<BrokeredMessage> ObservableFromAsyncReceiverDetails(Func<bool> receiverIsClosed, Action receiverClose, Func<Task<BrokeredMessage>> receiveMessage, IScheduler scheduler)
+		{
+			IObservable<BrokeredMessage> brokeredMessages = Observable.Create<BrokeredMessage>(
+				observer =>
+				{
 					Func<IScheduler, CancellationToken, Task<IDisposable>> messagePump = null;
 
 					messagePump = async (previousScheduler, cancellationToken) =>
 					{
 						cancellationToken.ThrowIfCancellationRequested();
 
-						if(messageReceiver.IsClosed)
+						if(receiverIsClosed())
 						{
 							observer.OnCompleted();
 
@@ -59,7 +93,7 @@ namespace HackedBrain.WindowsAzure.ServiceBus.Messaging
 						{
 							try
 							{
-								BrokeredMessage nextMessage = await receiveCall();
+								BrokeredMessage nextMessage = await receiveMessage();
 
 								observer.OnNext(nextMessage);
 							}
@@ -69,13 +103,13 @@ namespace HackedBrain.WindowsAzure.ServiceBus.Messaging
 							}
 
 							return ImmediateScheduler.Instance.ScheduleAsync(messagePump);
-						}						
+						}
 					};
 
 					return scheduler.ScheduleAsync(messagePump);
 				});
 
-			return Observable.Using(() => Disposable.Create(() => messageReceiver.Close()), _ => brokeredMessages);
+			return Observable.Using(() => Disposable.Create(receiverClose), _ => brokeredMessages);
 		}
 	}
 }
